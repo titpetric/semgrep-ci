6ac8f78040003ce0f6704c1a905c66f88253a27e
Tit Petric <tit@tyk.io>
2023-05-27 18:27:44 +0200

    netlify

--- site/build.sh
+++ site/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
+
+# install task
 GOBIN=/usr/local/bin go install github.com/go-task/task/v3/cmd/task@latest
 
+# clone themes
 cd themes && task && cd -
 
+# exec hugo build
 exec hugo --gc --minify
