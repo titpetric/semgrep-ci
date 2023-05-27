f420b932c4c90ed2ab43f60e48fcfefcd71c4bc7
Tit Petric <tit@tyk.io>
2023-05-27 18:30:57 +0200

    Try local bin folder for task, add to PATH

--- site/build.sh
+++ site/build.sh
@@ -2,7 +2,8 @@
 set -e
 
 # install task
-GOBIN=/usr/local/bin go install github.com/go-task/task/v3/cmd/task@latest
+GOBIN=./bin go install github.com/go-task/task/v3/cmd/task@latest
+export PATH=$PATH:/opt/build/repo/site/bin
 
 # clone themes
 cd themes && task && cd -
