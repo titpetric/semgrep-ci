5360fb78534d71c038e253f6649bc999d02eda53
Tit Petric <tit@tyk.io>
2023-05-27 18:22:33 +0200

    install task to go bin

--- netlify.sh
+++ netlify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-go install github.com/go-task/task/v3/cmd/task@latest
+GOBIN=/usr/local/bin go install github.com/go-task/task/v3/cmd/task@latest
 
 cd site/themes && task && cd -
 
