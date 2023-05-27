19d7e24df1cdd3e6cdf7f6b6261c2f781630f2e7
Tit Petric <tit@tyk.io>
2023-05-27 18:37:43 +0200

    what do you mean, exit 2

--- site/build.sh
+++ site/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # install task
 GOBIN=/opt/build/repo/site/bin go install github.com/go-task/task/v3/cmd/task@latest
@@ -10,3 +9,5 @@ cd themes && task && cd -
 
 # run hugo build
 hugo --gc --minify
+
+exit 0
